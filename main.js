eba99650473f4726d301adcd9c1313891ec76967
Nicholas <romitinicholas@gmail.com>
2021-11-08 17:08:15 +0100

    seconda soluzione

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 const toggleButton = document.querySelector(".header-right>a i");
 console.log(toggleButton);
 
-toggleButton.addEventListener("click", function(){
-    document.querySelector(".hamburger-menu").style.display = "block";
+// ********* prima soluzione *******
+// toggleButton.addEventListener("click", function(){
+//     document.querySelector(".hamburger-menu").style.display = "block";
 
+// });
+
+
+// *********** seconda soluzione *********
+toggleButton.addEventListener("click", function(){
+    document.querySelector(".hamburger-menu").className = "hamburger-menu active";
 });
 
 const toggleButtonClose = document.querySelector(".close i");
 console.log(toggleButtonClose);
 
+// ************ prima soluzione *********
+// toggleButtonClose.addEventListener("click", function(){
+//     document.querySelector(".hamburger-menu").style.display = "none";
+
+// });
+
+// *********** seconda soluzione **********
 toggleButtonClose.addEventListener("click", function(){
-    document.querySelector(".hamburger-menu").style.display = "none";
+    document.querySelector(".hamburger-menu").className = "hamburger-menu";
 
 });
